d5ab6eb9c11f2f08b39dea673de2ed2e19f8a2a6
zhuyihenzheng <wc2008kkll@163.com>
2024-11-03 02:01:34 +0900

    Update autocheck.py

--- autocheck.py
+++ autocheck.py
@@ -17,7 +17,7 @@ options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 options.add_argument("--headless")  # 无头模式，后台运行
-driver_path = "/usr/local/bin/chromedriver"
+driver_path = "/usr/local/bin/chromedriver-linux64/chromedriver"
 driver = webdriver.Chrome(executable_path=driver_path, options=options)
 
 def fetch_discounted_products():
